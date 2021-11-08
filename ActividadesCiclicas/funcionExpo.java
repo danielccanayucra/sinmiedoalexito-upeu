0c9848569d206613b634cb6b510da4ad422d4c36
danielccanayucr <>
2021-11-08 05:00:43 +0000

    areglo2

--- ActividadesCiclicas/funcionExpo.java
+++ ActividadesCiclicas/funcionExpo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ejemplo{
+public class funcionExpo{
    public static void main(String [] arg){
    Scanner sc = new Scanner(System.in);
    //datos de entrada
